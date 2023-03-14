97ffb0785dcfa87d73147b367f5dbc90fd420a2b
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-13 23:42:00 -0400

    Fully implementing fully obscured radar.

--- Resources/art/frontend/scrooby/ingame.lua
+++ Resources/art/frontend/scrooby/ingame.lua
@@ -1,5 +1,7 @@
-if Settings.RadarTopStyle == 1 then
-	Redirect("/GameData/art/frontend/scrooby/ingame_radar_normal.p3d")
-elseif Settings.RadarTopStyle == 2 then
-	Redirect("/GameData/art/frontend/scrooby/ingame_radar_obscured.p3d")
+if Settings.HitAndRunMeterStyle == 1 then
+	Redirect("/GameData/art/frontend/scrooby/ingame_hnrm_normal.p3d")
+elseif Settings.HitAndRunMeterStyle == 2 then
+	Redirect("/GameData/art/frontend/scrooby/ingame_hnrm_partially_obscured.p3d")
+elseif Settings.HitAndRunMeterStyle == 3 then
+	Redirect("/GameData/art/frontend/scrooby/ingame_hnrm_fully_obscured.p3d")
 end
