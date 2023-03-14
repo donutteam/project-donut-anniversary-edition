ae5d31dc5b8f176e821379b73d35ad8ca689df93
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-13 23:14:12 -0400

    Adding Hit & Run Meter style setting.

---
+++ CustomFiles.lua
@@ -0,0 +1,5 @@
+--
+-- Globals
+--
+
+Settings = GetSettings()
---
+++ Resources/path-handlers/art_frontend_ingame.lua
@@ -0,0 +1,5 @@
+if Settings.RadarTopStyle == 1 then
+	Redirect("/GameData/art/frontend/scrooby/ingame_radar_normal.p3d")
+elseif Settings.RadarTopStyle == 2 then
+	Redirect("/GameData/art/frontend/scrooby/ingame_radar_obscured.p3d")
+end
