c918f9073eac64b8cd06de7893d28e907cdf4b99
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-18 15:30:31 -0400

    Breaking up CustomFiles.lua.

--- CustomFiles.lua
+++ CustomFiles.lua
@@ -1,4 +1,10 @@
 --
+-- Get Settings
+--
+
+Settings = GetSettings()
+
+--
 -- Imports
 --
 
@@ -7,82 +13,5 @@ dofile(GetModPath() .. "/Resources/modules/P3D2.lua")
 
 P3D.LoadChunks(GetModPath() .. "/Resources/modules/P3DChunks")
 
---
--- Get Settings
---
-
-Settings = GetSettings()
-
---
--- Pick Hit & Run Car Model
---
-
-local HitAndRunCarModels =
-{
-	"cHears",
-	"hjackpol",
-	"miniufo",
-	"cPolice",
-}
-
-HitAndRunCarModel = HitAndRunCarModels[Settings.HitAndRunCarModel]
-
---
--- Load Alternate Sprites
---
-
-local function LoadSprites(newName, p3dFilePath, sprites)
-    local spritesP3DFile = P3D.P3DFile(p3dFilePath)
-
-    for chunk in spritesP3DFile:GetChunks(P3D.Identifiers.Sprite) do
-        local originalName = chunk.Name
-
-        chunk.Name = newName
-
-        sprites[originalName] = chunk
-    end
-end
-
-ErrorSprites = {}
-LoadSprites("error.png", GetModPath() .. "/Resources/art/frontend/sprites/error.p3d", ErrorSprites)
-
-RadarSprites = {}
-LoadSprites("radar.png", GetModPath() .. "/Resources/art/frontend/sprites/radar.p3d", RadarSprites)
-
-RadarTopSprites = {}
-LoadSprites("radartop.png", GetModPath() .. "/Resources/art/frontend/sprites/radartop.p3d",	RadarTopSprites)
-
---
--- P3D Replacements
---
-
-P3DChunkReplacements =
-{
-	["art//frontend//scrooby//bootup.p3d"] = {},
-	["art//frontend//scrooby//ingame.p3d"] = {},
-}
-
-if Settings.ErrorIconStyle == 1 then
-	P3DChunkReplacements["art//frontend//scrooby//bootup.p3d"]["error.png"] = ErrorSprites["error_pink_donut.png"]
-	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["error.png"] = ErrorSprites["error_pink_donut.png"]
-elseif Settings.ErrorIconStyle == 2 then
-	-- TODO: Replace with Tyler's Icon
-elseif Settings.ErrorIconStyle == 3 then
-	-- Do Nothing
-end
-
-if Settings.RadarStyle == 1 then
-	-- Do Nothing
-elseif Settings.RadarStyle == 2 then
-	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radar.png"] = RadarSprites["radar_blue.png"]
-elseif Settings.RadarStyle == 3 then
-	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radar.png"] = RadarSprites["radar_red.png"]
-end
-
-if Settings.RadarTopStyle == 1 then
-	-- Do Nothing
-elseif Settings.RadarTopStyle == 2 then
-	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radartop.png"] = RadarTopSprites["radartop_partially_obscured.png"]
-elseif Settings.RadarTopStyle == 3 then
-	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radartop.png"] = RadarTopSprites["radartop_fully_obscured.png"]
-end
+dofile(GetModPath() .. "/Resources/scripts/data/HitAndRunCarModel.lua")
+dofile(GetModPath() .. "/Resources/scripts/data/P3DChunkReplacements.lua")
---
+++ Resources/scripts/data/HitAndRunCarModel.lua
@@ -0,0 +1,13 @@
+--
+-- Pick Hit & Run Car Model
+--
+
+local HitAndRunCarModels =
+{
+	"cHears",
+	"hjackpol",
+	"miniufo",
+	"cPolice",
+}
+
+HitAndRunCarModel = HitAndRunCarModels[Settings.HitAndRunCarModel]
