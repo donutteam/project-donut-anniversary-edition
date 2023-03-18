0dcaf0eb87a53cc5e4ff7416a684de15b6bf1dba
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-18 13:19:37 -0400

    Adding Radar Style setting.

--- CustomFiles.lua
+++ CustomFiles.lua
@@ -8,10 +8,14 @@ dofile(GetModPath() .. "/Resources/modules/P3D2.lua")
 P3D.LoadChunks(GetModPath() .. "/Resources/modules/P3DChunks")
 
 --
--- Globals
+-- Get Settings
 --
 
 Settings = GetSettings()
+
+--
+-- Pick Hit & Run Car Model
+--
 
 local HitAndRunCarModels =
 {
@@ -23,14 +27,49 @@ local HitAndRunCarModels =
 
 HitAndRunCarModel = HitAndRunCarModels[Settings.HitAndRunCarModel]
 
+--
+-- Load Alternate Sprites
+--
+
+local function LoadSprites(newName, p3dFilePath, sprites)
+    local spritesP3DFile = P3D.P3DFile(p3dFilePath)
+
+    for chunk in spritesP3DFile:GetChunks(P3D.Identifiers.Sprite) do
+        local originalName = chunk.Name
+
+        chunk.Name = newName
+
+        sprites[originalName] = chunk
+    end
+end
+
+ErrorSprites = {}
+LoadSprites("error.png", GetModPath() .. "/Resources/art/frontend/sprites/error.p3d", ErrorSprites)
+
+RadarSprites = {}
+LoadSprites("radar.png", GetModPath() .. "/Resources/art/frontend/sprites/radar.p3d", RadarSprites)
+
 RadarTopSprites = {}
+LoadSprites("radartop.png", GetModPath() .. "/Resources/art/frontend/sprites/radartop.p3d",	RadarTopSprites)
 
-local RadarTopSpritesP3DFile = P3D.P3DFile(GetModPath() .. "/Resources/art/frontend/sprites/radartop.p3d")
+--
+-- P3D Replacements
+--
 
-for chunk in RadarTopSpritesP3DFile:GetChunks(P3D.Identifiers.Sprite) do
-	local originalName = chunk.Name
+P3DChunkReplacements =
+{
+	["art//frontend//scrooby//ingame.p3d"] =
+	{
+		["error.png"] = ErrorSprites["error_donut.png"],
+	}
+}
 
-	chunk.Name = "radartop.png"
+if Settings.RadarStyle == 2 then
+	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radar.png"] = RadarSprites["radar_red.png"]
+end
 
-    RadarTopSprites[originalName] = chunk
+if Settings.RadarTopStyle == 2 then
+	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radartop.png"] = RadarTopSprites["radartop_partially_obscured.png"]
+elseif Settings.RadarTopStyle == 3 then
+	P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]["radartop.png"] = RadarTopSprites["radartop_fully_obscured.png"]
 end
--- Resources/art/frontend/scrooby/ingame.lua
+++ Resources/art/frontend/scrooby/ingame.lua
@@ -1,27 +1,11 @@
-if Settings.HitAndRunMeterStyle == 1 then
-	return
-end
+local IngameChunkReplacements = P3DChunkReplacements["art//frontend//scrooby//ingame.p3d"]
 
 local IngameP3DFile = P3D.P3DFile("/GameData/art/frontend/scrooby/ingame.p3d")
 
-local ChunkIndex
-
 for index, chunk in IngameP3DFile:GetChunksIndexed(P3D.Identifiers.Sprite) do
-    if chunk.Name == "radartop.png" then
-        ChunkIndex = index
-		
-        break
-    end
-end
-
-if not ChunkIndex then
-    return
-end
-
-if Settings.HitAndRunMeterStyle == 2 then
-	IngameP3DFile.Chunks[ChunkIndex] = RadarTopSprites["radartop_partially_obscured.png"]
-elseif Settings.HitAndRunMeterStyle == 3 then
-	IngameP3DFile.Chunks[ChunkIndex] = RadarTopSprites["radartop_fully_obscured.png"]
+	if IngameChunkReplacements[chunk.Name] then
+		IngameP3DFile.Chunks[index] = IngameChunkReplacements[chunk.Name]
+	end
 end
 
 IngameP3DFile:Output()
