0e59d14c651b3a3a26b1793894424b4f7fdf7dfc
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-27 23:47:17 -0400

    Converting SR2 to Lua.

---
+++ Resources/scripts/missions/level01/sr2l.lua
@@ -0,0 +1,8 @@
+Game.LoadDisposableCar("art\\cars\\honor_v.p3d","honor_v","AI")
+
+Game.LoadP3DFile("art\\frontend\\dynaload\\images\\msnicons\\object\\race.p3d")
+
+Game.LoadP3DFile("art\\locators\\level01\\sr2.p3d")
+
+Game.LoadP3DFile("art\\missions\\generic\\fline.p3d")
+Game.LoadP3DFile("art\\missions\\level01\\race2cam.p3d")
