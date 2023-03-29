e9eea6721ee5713262e039ab0c1b94ada477760e
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-28 23:18:19 -0400

    Moving M7's locators file.

--- Resources/scripts/missions/level01/m7l.lua
+++ Resources/scripts/missions/level01/m7l.lua
@@ -1,11 +1,11 @@
-Game.LoadP3DFile("art\\missions\\level01\\m7.p3d")
-
-Game.LoadDisposableCar("art\\cars\\smithalt.p3d","smithalt","AI")
-Game.LoadDisposableCar("art\\cars\\cVan.p3d", "cvan","AI" )
 Game.LoadDisposableCar("art\\cars\\cDonut.p3d","cDonut","AI")
 Game.LoadDisposableCar("art\\cars\\cLimo.p3d", "cLimo","AI")
+Game.LoadDisposableCar("art\\cars\\cVan.p3d", "cvan","AI")
+Game.LoadDisposableCar("art\\cars\\smithalt.p3d","smithalt","AI")
+
+Game.LoadP3DFile("art\\frontend\\dynaload\\images\\msnicons\\char\\burns.p3d")
+Game.LoadP3DFile("art\\frontend\\dynaload\\images\\msnicons\\location\\mansion.p3d")
+
+Game.LoadP3DFile("art\\locators\\level01\\m7.p3d")
 
 Game.LoadP3DFile("art\\missions\\generic\\fline.p3d")
-
-Game.LoadP3DFile("art\\frontend\\dynaload\\images\\msnicons\\location\\mansion.p3d")
-Game.LoadP3DFile("art\\frontend\\dynaload\\images\\msnicons\\char\\burns.p3d")
