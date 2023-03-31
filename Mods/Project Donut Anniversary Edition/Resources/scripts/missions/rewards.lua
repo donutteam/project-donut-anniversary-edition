904e62436b68e1aa14607e3f1328710ef1c3de0e
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-03-30 23:18:05 -0400

    Adding many outfits for Homer.

--- Mods/Project Donut Anniversary Edition/Resources/scripts/missions/rewards.lua
+++ Mods/Project Donut Anniversary Edition/Resources/scripts/missions/rewards.lua
@@ -13,15 +13,25 @@ Game.BindReward("donut_v",   "art\\cars\\donut_v.p3d",     "car", "forsale",    
 
 Game.BindReward("homer",     "art\\chars\\homer_m.p3d",    "skin", "defaultskin", 1)
 
-Game.BindReward("h_undrwr",  "art\\chars\\h_undr_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_fat",     "art\\chars\\h_fat_m.p3d",    "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_stcrobe", "art\\chars\\h_stcr_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_scuzzy",  "art\\chars\\h_scuz_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_evil",    "art\\chars\\h_evil_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_donut",   "art\\chars\\h_donu_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_undrwr",  "art\\chars\\h_undr_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_fat",     "art\\chars\\h_fat_m.p3d",    "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_stcrobe", "art\\chars\\h_stcr_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_scuzzy",  "art\\chars\\h_scuz_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_evil",    "art\\chars\\h_evil_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_donut",   "art\\chars\\h_donu_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
 
-Game.BindReward("h_dt2",     "art\\chars\\h_dt2_m.p3d",    "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
-Game.BindReward("h_work",    "art\\chars\\h_work_m.p3d",   "skin", "forsale",     1, 100 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_dt1",     "art\\chars\\h_dt1_m.p3d",    "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_dt2",     "art\\chars\\h_dt2_m.p3d",    "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_dt3",     "art\\chars\\h_dt3_m.p3d",    "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_elf",     "art\\chars\\h_elf_m.p3d",    "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_gold",    "art\\chars\\h_gold_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_inc1",    "art\\chars\\h_inc1_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_inc2",    "art\\chars\\h_inc2_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_mrplow",  "art\\chars\\h_mrpl_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_pieman",  "art\\chars\\h_piem_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_sick",    "art\\chars\\h_sick_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_sshield", "art\\chars\\h_sshi_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
+Game.BindReward("h_work",    "art\\chars\\h_work_m.p3d",   "skin", "forsale",     1, 50 * Settings.RewardPriceMultiplier, "interior")
 
 --                               Top Speed   Acceleration   Toughness   Handling
 Game.SetCarAttributes("famil_v", 1.0,        1.5,           2.5,        4.0)
